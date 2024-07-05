53edd3d6454dc482648d9588bb0779c8d10af816
Shivangi Mittal <54174324+shivangimittal791@users.noreply.github.com>
2024-07-05 14:13:35 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -28,28 +28,4 @@ function days(endDate, startDate) {
   return Math.floor(diffInMs / (1000 * 60 * 60 * 24));
 }
 
-function generateFunction() {
- window.alert('Hello World'); 
-}
-function generateFunction2() {
- window.alert('Hello World'); 
-}
-function generateFunction3() {
- window.alert('Hello World'); 
-}
-function generateFunction4() {
- window.alert('Hello World'); 
-}
-function generateFunction7() {
- window.alert('Hello World'); 
-}
-function generateFunction8() {
- window.alert('Hello World'); 
-}
-function generateFunction9() {
- window.alert('Hello World'); 
-}
-function generateFunction10() {
- window.alert('Hello World'); 
-}
-export { getFullName, days , generateFunction, generateFunction2, generateFunction3, generateFunction4, generateFunction7, generateFunction8, generateFunction9, generateFunction10};
+export { getFullName, days};
