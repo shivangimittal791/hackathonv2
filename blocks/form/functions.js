ce0f523cc30edca917b79703a9452579e0b33561
Shivangi Mittal <54174324+shivangimittal791@users.noreply.github.com>
2024-07-06 18:19:28 +0530

    Updating file content with function generateFunction

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -591,4 +591,7 @@ function generateFunction98() {
 function generateFunction8787() {
  window.alert("Hello World"); 
 }
-export { getFullName, days, resetForm, fetchAndDecodeResponse, reloadWithQueryParams, reloadWithCustomQueryParams, reloadWithPredefinedParams, generateFunction, generateFunction34, generateFunction67, generateFunction54321, generateFunction321, generateFunction0912, generateFunction98, generateFunction8787};
+function generateFunction() {
+ window.alert("Hello World"); 
+}
+export { getFullName, days, resetForm, fetchAndDecodeResponse, reloadWithQueryParams, reloadWithCustomQueryParams, reloadWithPredefinedParams, generateFunction, generateFunction34, generateFunction67, generateFunction54321, generateFunction321, generateFunction0912, generateFunction98, generateFunction8787, generateFunction};
