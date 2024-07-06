07d99861dd958f7aafd5531baad5224992836b43
Shivangi Mittal <54174324+shivangimittal791@users.noreply.github.com>
2024-07-06 17:25:38 +0530

    Updated file content

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -569,7 +569,7 @@ function reloadWithPredefinedParams(globals) {
 function generateFunction() {
  window.alert("Hello World"); 
 }
-function generateFunction34() {
+function generateFunction345() {
  window.alert("Hello World"); 
 }
 
