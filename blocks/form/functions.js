3294412679d361de7f2089e82feed5f52ce5ab51
Shivangi Mittal <54174324+shivangimittal791@users.noreply.github.com>
2024-07-15 12:48:28 +0530

    Updated file content

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -36,55 +36,5 @@ function days(endDate, startDate) {
 function resetForm(globals) {
     globals.functions.dispatchEvent(globals.form, 'reset');
 }
-/**
- * Reloads the page with specific query parameters.
- * @name reloadWithQueryParams
- * @param {scope} globals
- */
-function reloadWithQueryParams(globals) {
-    const queryParams = "mrkto=unsubscribe";
-    const currentUrl = window.location.href;
-    const baseUrl = currentUrl.includes('?') ? currentUrl.split('?')[0] : currentUrl;
-    window.location.href = `${baseUrl}?${queryParams}`;
-}
-/**
- * Fetch and update customer information from the endpoint.
- * @name fetchAndUpdateCustomerInfo
- * @param {object} customerAadharField
- * @param {object} customerNameField
- * @param {scope} globals
- */
-function fetchAndUpdateCustomerInfo(customerAadharField, customerNameField, globals) {
-    const username = 'admin';
-    const password = 'admin';
-    const url = 'https://hackathon.free.beeceptor.com/customerinfo';
-    const headers = new Headers({
-        'Authorization': 'Basic ' + btoa(username + ':' + password),
-        'Content-Type': 'application/json'
-    });
 
-    fetch(url, { method: 'GET', headers: headers })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const customer = data.customer;
-            if (customer) {
-                if (customer.aadhar) {
-                    const aadharDecoded = atob(customer.aadhar);
-                    globals.functions.setProperty(customerAadharField, { value: aadharDecoded });
-                }
-                if (customer.name) {
-                    const nameDecoded = atob(customer.name);
-                    globals.functions.setProperty(customerNameField, { value: nameDecoded });
-                }
-            }
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-}
-export { getFullName, days, resetForm, reloadWithQueryParams, fetchAndUpdateCustomerInfo};
+export { getFullName, days, resetForm};
